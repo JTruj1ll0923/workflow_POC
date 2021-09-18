484d3fcd42ecf907b98a018a73f897875e89d375
Justin Trujillo <jtrujillo923@gmail.com>
2021-09-18 14:53:56 -0600

    Allow for uncreated workflow stoppers and allow for resolved statuses"

--- main.py
+++ main.py
@@ -2,10 +2,29 @@
 import pandas as pd
 
 
+class Notes:
+    def __init__(self):
+        pass
+
+    steps = []
+    notes = []
+
+    def add_notes(self, note_to_add):
+        self.notes.append(note_to_add)
+
+    def print_notes(self):
+        for note in self.notes:
+            print(note)
+
+
 def step(data, option=""):
     choice = ""
     if option == "":
         option = "A00"
+    if data[option]["reason"] == "Resolved":
+        return "Resolved"
+    elif data[option]["reason"] == "Not Created Yet":
+        return "Not Created Yet"
     print(data[option]["process"])
     # print(data[option]["reason"])
     option1 = str(data[option]["option1"]).split(sep=";")
@@ -23,12 +42,15 @@ def step(data, option=""):
 
 def main():
     data = pd.read_json("steps.json")
+    notes = Notes()
     resolved = False
+    resolutions = ["Resolved", "Not Created Yet"]
     choice = ""
     while not resolved:
         choice = step(data, choice)
-    # print(data["A02A"]["process"])
-    # print(data)
+        if choice in resolutions:
+            resolved = True
+            notes.print_notes()
 
 
 if __name__ == '__main__':
